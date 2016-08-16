5cdf336313036d5d7667d23cb7fca42b559dc720
Srimal Samansiri <srimalsl@yahoo.com>
2016-08-16 10:50:27 +0000

    Initial Add

---
+++ polls/models.py
@@ -0,0 +1,19 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	def _str_(self):
+		return self.question_text
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	def _str_(self):
+		return self.choice_text
